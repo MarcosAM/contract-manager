bd3eff0417f8fa4c38960225ff10aff743fdfebe
DESKTOP-K49PGQT\MarcosMourão <marcosarruda.m@gmail.com>
2019-07-14 12:20:24 -0300

    feature/list page loads information through json server

--- contracts-manager/src/pages/ListPage.js
+++ contracts-manager/src/pages/ListPage.js
@@ -1,50 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { bindActionCreators } from 'redux'
+import * as contractsActions from '../actions/contractsActions'
+import { connect } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles'
 import CustomList from '../components/CustomList'
-import { New, Preparing, Ready, Done } from '../constants/contractsStates'
 
 
-const INITIAL_STATE = [
-    {
-        id: '01',
-        state: New,
-        title: 'Cliente 01',
-        details: 'Altas descrições de pedidos'
-    },
-    {
-        id: '02',
-        state: Preparing,
-        title: 'Cliente 02',
-        details: 'Altas descrições de pedidos'
-    },
-    {
-        id: '03',
-        state: Ready,
-        title: 'Cliente 03',
-        details: 'Altas descrições de pedidos'
-    },
-    {
-        id: '04',
-        state: Done,
-        title: 'Cliente 04',
-        details: 'Altas descrições de pedidos'
-    },
-]
-
-
-export default function SimpleList() {
-    const classes = useStyles();
+const ListPage = ({ contracts, loadContracts }) => {
+    const { root } = useStyles();
 
     const convertContractsToListItems = (contracts) => (
         contracts.map(({ id, title }) => ({ primary: title, secondary: id }))
     )
 
+    useEffect(() => loadContracts(), [])
+
     return (
-        <div className={classes.root}>
-            <CustomList items={convertContractsToListItems(INITIAL_STATE)} />
+        <div className={root}>
+            <CustomList items={convertContractsToListItems(contracts)} />
         </div>
     );
 }
+
+const mapStateToProps = ({ contracts }) => ({ contracts })
+
+const mapDispatchToProps = dispatch => (bindActionCreators(contractsActions, dispatch))
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListPage)
 
 const useStyles = makeStyles(theme => ({
     root: {
--- contracts-manager/src/reducers/contractsReducer.js
+++ contracts-manager/src/reducers/contractsReducer.js
@@ -1,33 +1,4 @@
-import { New, Preparing, Ready, Done } from '../constants/contractsStates'
 import * as types from '../actions/actionTypes'
-/*
-const INITIAL_STATE = [
-    {
-        id: '01',
-        state: New,
-        title: 'Cliente 01',
-        details: 'Altas descrições de pedidos'
-    },
-    {
-        id: '02',
-        state: Preparing,
-        title: 'Cliente 02',
-        details: 'Altas descrições de pedidos'
-    },
-    {
-        id: '03',
-        state: Ready,
-        title: 'Cliente 03',
-        details: 'Altas descrições de pedidos'
-    },
-    {
-        id: '04',
-        state: Done,
-        title: 'Cliente 04',
-        details: 'Altas descrições de pedidos'
-    },
-]
-*/
 
 
 export default (state = [], action) => {
